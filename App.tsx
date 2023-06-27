4acdf863791b0cf1063be36a80c5ad217e20b871
Charlotte Cutler <charlottecutler1@hotmail.co.uk>
2023-06-27 14:07:12 +0100

    Add typescript to project

---
+++ App.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, View } from 'react-native';
+import React from 'react';
+
+export default function App() {
+
+  return (
+    <View style={styles.container}>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {}
+});
